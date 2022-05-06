4c0b834a8d06146fde58956d78eaf0897abebb5b
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-05-06 22:04:42 +0200

    tweak: run world.ts in async and update deps

--- client/world.ts
+++ client/world.ts
@@ -1,30 +1,31 @@
 import config from "../config.json";
-import { Delay } from "./utils";
 
-if (config.world.disableWantedLevel) {
-	ClearPlayerWantedLevel(PlayerPedId());
-	SetMaxWantedLevel(0);
-}
-
-if (config.world.enablePvP) {
-	SetCanAttackFriendly(PlayerPedId(), true, false);
-	NetworkSetFriendlyFireOption(true);
-}
-
-if (config.world.calmAI) {
-	SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_HILLBILLY"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_BALLAS"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_MEXICAN"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_FAMILY"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_MARABUNTE"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_SALVA"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("GANG_1"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("GANG_2"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("GANG_9"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("GANG_10"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("FIREMAN"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("MEDIC"), GetHashKey("PLAYER"));
-	SetRelationshipBetweenGroups(1, GetHashKey("COP"), GetHashKey("PLAYER"));
-}
-
-if (config.world.disableDistantSirens) DistantCopCarSirens(!config.world.disableDistantSirens);
+setImmediate(() =>{
+	if (config.world.disableWantedLevel) {
+		ClearPlayerWantedLevel(PlayerPedId());
+		SetMaxWantedLevel(0);
+	}
+	
+	if (config.world.enablePvP) {
+		SetCanAttackFriendly(PlayerPedId(), true, false);
+		NetworkSetFriendlyFireOption(true);
+	}
+	
+	if (config.world.calmAI) {
+		SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_HILLBILLY"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_BALLAS"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_MEXICAN"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_FAMILY"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_MARABUNTE"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_SALVA"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("GANG_1"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("GANG_2"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("GANG_9"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("GANG_10"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("FIREMAN"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("MEDIC"), GetHashKey("PLAYER"));
+		SetRelationshipBetweenGroups(1, GetHashKey("COP"), GetHashKey("PLAYER"));
+	}
+	
+	if (config.world.disableDistantSirens) DistantCopCarSirens(!config.world.disableDistantSirens);
+});
