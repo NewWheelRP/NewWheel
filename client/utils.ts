3fa5917d9601785c1c4c0c476de6b37ab443203d
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-09-21 00:54:00 +0200

    feat: joaat

--- client/utils.ts
+++ client/utils.ts
@@ -39,3 +39,22 @@ export const getClosestVehicle = (coords: Vector3): number => {
 
 	return currentVeh;
 };
+
+export const joaat = (key: string): number => {
+    const keyLowered = key.toLowerCase();
+    const length = keyLowered.length;
+
+    let hash, i;
+
+    for (hash = i = 0; i < length; i++) {
+        hash += keyLowered.charCodeAt(i);
+        hash += hash << 10;
+        hash ^= hash >>> 6;
+    }
+
+    hash += hash << 3;
+    hash ^= hash >>> 11;
+    hash += hash << 15;
+
+    return Math.floor(hash << 0);
+};
